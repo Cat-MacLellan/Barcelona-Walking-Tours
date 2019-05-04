bcb7bd8c07015c084c4e30ec374412c9db47c7ee
Cat MacLellan <catrionamac@hotmail.co.uk>
2019-05-04 19:36:08 +0000

    smooth scroll added and navbar shrink amended

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
+window.onscroll = function() { scrollFunction() };
 
-window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    document.getElementById("logo").style.height= "50px";
-  } else {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("logo").style.height = "50px";
+  }
+  else {
     document.getElementById("logo").style.height = "80px";
   }
 }
+
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function() {
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
