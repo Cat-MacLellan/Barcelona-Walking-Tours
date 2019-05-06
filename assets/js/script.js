cbe2cde4c31e49c49f50148563f0f364dc5b09c6
Cat MacLellan <catrionamac@hotmail.co.uk>
2019-05-06 22:07:22 +0000

    Tour intro added/ attempt at padding between sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,49 @@ $(document).ready(function() {
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 900, function() {
+      }, 1000, function() {
         window.location.hash = hash;
       });
-    } // End if
+    }
   });
+});
+$('a').click(function() {
+  $('.navbar-collapse').collapse('hide');
+});
+
+
+$('.tour-one').mouseenter(function() {
+  $('.tour-one-txt').fadeIn('slow')
+    .css('display', 'block');
+});
+$('.tour-one').mouseleave(function() {
+  $('.tour-one-txt').fadeOut('slow');
+});
+$('.tour-two').mouseenter(function() {
+  $('.tour-two-txt').fadeIn('slow')
+    .css('display', 'block');
+});
+$('.tour-two').mouseleave(function() {
+  $('.tour-two-txt').fadeOut('slow');
+});
+$('.tour-three').mouseenter(function() {
+  $('.tour-three-txt').fadeIn('slow')
+    .css('display', 'block');
+});
+$('.tour-three').mouseleave(function() {
+  $('.tour-three-txt').fadeOut('slow');
+});
+$('.tour-four').mouseenter(function() {
+  $('.tour-four-txt').fadeIn('slow')
+    .css('display', 'block');
+});
+$('.tour-four').mouseleave(function() {
+  $('.tour-four-txt').fadeOut('slow');
+});
+$('.tour-five').mouseenter(function() {
+  $('.tour-five-txt').fadeIn('slow')
+    .css('display', 'block');
+});
+$('.tour-five').mouseleave(function() {
+  $('.tour-five-txt').fadeOut('slow');
 });
