efb8a6a9b1940391c04b771c16b40a5a69c38f2d
Cat MacLellan <catrionamac@hotmail.co.uk>
2019-05-13 20:06:19 +0000

    line between locations added to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,21 +6,31 @@ function initMap() {
             lng: 2.173504
         }
     });
+    var line = new google.maps.Polyline({
+        path: [
+            new google.maps.LatLng(41.403706, 2.173504),
+            new google.maps.LatLng(41.4145, 2.1527)
+        ],
+        strokeColor: "#FFBD59",
+        strokeOpacity: 1.0,
+        strokeWeight: 5,
+        map: map
+    });
 
     var locations = [{
-            lat: 41.403706,
-            lng: 2.173504
-        }, {
-            lat: 41.4145,
-            lng: 2.1527
+        lat: 41.403706,
+        lng: 2.173504
+    }, {
+        lat: 41.4145,
+        lng: 2.1527
     }];
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
 
+        });
     });
-});
-var markerCluster = new MarkerClusterer(map, markers, {
-    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-});
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
 }
