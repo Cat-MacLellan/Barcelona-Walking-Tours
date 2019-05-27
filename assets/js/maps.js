e1e40220f5325a7ce3eb6cddfdf6c2af29c23aeb
Cat MacLellan <catrionamac@hotmail.co.uk>
2019-05-27 14:40:55 +0000

    Google Map Added with data and Markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,37 +1,94 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("gaudiMap"), {
-        zoom: 12,
-        center: {
-            lat: 41.403706,
-            lng: 2.173504
-        }
-    });
-    var line = new google.maps.Polyline({
-        path: [
-            new google.maps.LatLng(41.403706, 2.173504),
-            new google.maps.LatLng(41.4145, 2.1527)
-        ],
-        strokeColor: "#FFBD59",
-        strokeOpacity: 1.0,
-        strokeWeight: 5,
-        map: map
-    });
+let map;
+let markers = [];
+let infoWindow;
 
-    var locations = [{
-        lat: 41.403706,
-        lng: 2.173504
-    }, {
-        lat: 41.4145,
-        lng: 2.1527
-    }];
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
+let locations = [{
+        tour: 'gaudi',
+        latlng: { lat: 41.403706, lng: 2.173504 },
+    },
+    {
+        tour: 'gaudi',
+        latlng: { lat: 41.4145, lng: 2.1527 },
+    },
+    {
+        tour: 'gothic',
+        latlng: { lat: 41.3839, lng: 2.1821 },
+    },
+    {
+        tour: 'gothic',
+        latlng: { lat: 41.3840, lng: 2.1762 },
+    },
+    {
+        tour: 'culture',
+        latlng: { lat: 41.3744, lng: 2.1696 },
+    },
+    {
+        tour: 'culture',
+        latlng: { lat: 41.3876, lng: 2.1753 },
+    },
+    {
+        tour: 'transport',
+        latlng: { lat: 41.3840, lng: 2.1865 },
+    },
+    {
+        tour: 'transport',
+        latlng: { lat: 41.370556, lng: 2.170556 },
+    },
+    {
+        tour: 'thefuture',
+        latlng: { lat: 41.402472, lng: 2.194556 },
+    },
+    {
+        tour: 'thefuture',
+        latlng: { lat: 41.411492, lng: 2.228094 },
+    }
+];
+
+
+function initMap() {
+    let options = {
+        center: new google.maps.LatLng(41.3851, 2.1734),
+        zoom: 12
+    };
+    map = new google.maps.Map(document.getElementById('map'), options);
+    infoWindow = new google.maps.InfoWindow();
+
+
+
+    const addmarker = function(args) {
+        let mkr = new google.maps.Marker({
+            position: args.latlng,
+            map: map
 
         });
-    });
-    var markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        if (args.hasOwnProperty('icon')) mkr.setIcon(args.icon);
+        if (args.hasOwnProperty('name')) mkr.name = args.name;
+        if (args.hasOwnProperty('content')) mkr.content = args.content;
+
+        google.maps.event.addListener(mkr, 'click', clickhandler);
+        return mkr;
+    };
+    const clickhandler = function(e) {
+        infoWindow.open(map, this);
+        infoWindow.setContent(this.content);
+    };
+    const clearmarkers = function() {
+        markers.forEach(mkr => {
+            mkr.setMap(null);
+        });
+    };
+
+    Array.prototype.slice.call(document.querySelectorAll('button[type="radio"][name="tour"]')).forEach(function(input) {
+        input.addEventListener('click', function(e) {
+            if (this.value) {
+                /* clear any markers added to the map already */
+                clearmarkers();
+
+                /* only show those that qualify based upon selected tour */
+                locations.forEach(obj => {
+                    if (obj.tour == this.value) markers.push(addmarker.call(this, obj));
+                });
+            }
+        });
     });
 }
-
